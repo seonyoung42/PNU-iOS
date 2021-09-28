54b970bf86a8af3f6d2b9891b8accac02b68ed04
seonyoung <4tjsdud2@naver.com>
2021-09-28 17:35:45 +0900

    Upload image Array to Azure

--- ProjectH/ProjectH/ViewController.swift
+++ ProjectH/ProjectH/ViewController.swift
@@ -13,13 +13,30 @@ class ViewController: UIViewController {
     var imgV: UIImageView!
     var btnA: UIButton!
     var btnB: UIButton!
-    var imagArr: [UIImage]!
+    
+    var imagArr = [UIImage]()
+    var imagName = [String]()
+    var imageCount = 0
+//    {
+//        didSet {
+//            DispatchQueue.main.async {
+//                self.imgV.image = self.imagArr[self.imageCount]
+//            }
+//        }
+//    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        imagName = ["03750020.JPG","03750026.JPG","03750027.JPG","03750028.JPG","03750037.JPG"]
+        
+        imagName.forEach {
+            imagArr.append(UIImage(named: $0)!)
+        }
+        
         self.imgV = UIImageView(frame: CGRect(x: 50, y: 50, width: (self.view.bounds.width)-100, height: (self.view.bounds.height)-500))
-        self.imgV.image = UIImage(named: "Group 37")
+//        self.imgV.image = UIImage(named: "03750020")
+        self.imgV.image = imagArr[imageCount]
         self.imgV.layer.cornerRadius = 10 // > 이미지 테두리 둥글게
         self.imgV.clipsToBounds = true  // > 이걸 해야 테두리 둥글게가 보임
         self.imgV.layer.borderWidth = 2.0
@@ -39,37 +56,53 @@ class ViewController: UIViewController {
         self.view.addSubview(self.btnB)
         
         
+    
+//        imagArr.append(UIImage(named: "03750020.JPG")!)
+//        imagArr.append(UIImage(named: "03750026.JPG")!)
+//        imagArr.append(UIImage(named: "03750027.JPG")!)
+//        imagArr.append(UIImage(named: "03750028.JPG")!)
+//        imagArr.append(UIImage(named: "03750037.JPG")!)
+
     }
     
     @objc func uploadIMG() {
-        let account: AZSCloudStorageAccount = try! AZSCloudStorageAccount(fromConnectionString: "DefaultEndpointsProtocol=https;AccountName=seonyoungsto;AccountKey=Nof9gevDCnpOdQSdqB3jiddfOuRWMzlgsXid3uJP9SZZZJAlfpKKaUgpOMzBkvk9MpU8BpvZ7xlFqr159e3N7A==;EndpointSuffix=core.windows.net")
+        let account: AZSCloudStorageAccount = try! AZSCloudStorageAccount(fromConnectionString: "DefaultEndpointsProtocol=https;AccountName=seonyoungsto;AccountKey=16F0hD392SntRjUUJsZPw479bKNeQ7ibZg/YGj7WSe9f97D/9ja6qKjGuIzPmKJb9KKaaOchoo5hTDe3GubYzw==;EndpointSuffix=core.windows.net")
+    
         
 //        let cred: AZSStorageCredentials = AZSStorageCredentials(accountName: "seonyoungsto", accountKey: <#T##String#>)
         
         let blobClient: AZSCloudBlobClient = account.getBlobClient()
         let blobContainer: AZSCloudBlobContainer = blobClient.containerReference(fromName: "imgcon")
-        let blockBlob: AZSCloudBlockBlob = blobContainer.blockBlobReference(fromName: "Group 37.png")
+        let blockBlob: AZSCloudBlockBlob = blobContainer.blockBlobReference(fromName: imagName[imageCount])
+//        let blockBlob: AZSCloudBlob = blobContainer.blockBlobReference(fromName: self.imgV.image)
         
         let imgData = self.imgV.image!.pngData()
+        
         blockBlob.upload(from: imgData!) { errorA in
+            DispatchQueue.main.async {
+                self.imageCount += 1
+                self.imgV.image = self.imagArr[self.imageCount]
+            }
             print("OK")
+//            self.imageCount += 1
         }
     }
     
+    
     @objc func downloadIMG() {
-        let account: AZSCloudStorageAccount = try! AZSCloudStorageAccount(fromConnectionString: "DefaultEndpointsProtocol=https;AccountName=seonyoungsto;AccountKey=Nof9gevDCnpOdQSdqB3jiddfOuRWMzlgsXid3uJP9SZZZJAlfpKKaUgpOMzBkvk9MpU8BpvZ7xlFqr159e3N7A==;EndpointSuffix=core.windows.net")
+        let account: AZSCloudStorageAccount = try! AZSCloudStorageAccount(fromConnectionString: "DefaultEndpointsProtocol=https;AccountName=seonyoungsto;AccountKey=16F0hD392SntRjUUJsZPw479bKNeQ7ibZg/YGj7WSe9f97D/9ja6qKjGuIzPmKJb9KKaaOchoo5hTDe3GubYzw==;EndpointSuffix=core.windows.net")
         
 //        let cred: AZSStorageCredentials = AZSStorageCredentials(accountName: "seonyoungsto", accountKey: <#T##String#>)
         
         let blobClient: AZSCloudBlobClient = account.getBlobClient()
         let blobContainer: AZSCloudBlobContainer = blobClient.containerReference(fromName: "imgcon")
-        let blockBlob: AZSCloudBlockBlob = blobContainer.blockBlobReference(fromName: "bts.jpg")
+        let blockBlob: AZSCloudBlockBlob = blobContainer.blockBlobReference(fromName: "03750033.JPG")
                 
         blockBlob.downloadToData { err, dataA in
             let imgB: UIImage = UIImage(data: dataA!)!
             DispatchQueue.main.async() {
                 self.imgV.image = imgB
-                self.imagArr.append(imgB)
+//                self.imagArr.append(imgB)
             }
         }
     }
